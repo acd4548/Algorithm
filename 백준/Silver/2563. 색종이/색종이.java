54da6853b2c4a733d896d3f032bc17ae1e8810fc
MinKook YOU <122504425+acd4548@users.noreply.github.com>
2024-01-04 15:51:03 +0900

    [Silver V] Title: 색종이, Time: 76 ms, Memory: 11600 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,33 +1,30 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
-public class Main{
-	public static void main(String [] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int N = sc.nextInt();
-		int [][] arr = new int [100][100];
-		
-		for(int i = 0; i<N; i++) {
-			int a = sc.nextInt();
-			int b = sc.nextInt();
-			
-			for(int j = a; j<a+10; j++) {
-				for(int q = b; q<b+10; q++) {
-					arr[j][q] = 1;
+public class Main {
+	public static void main (String [] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int [][] map = new int [100][100];
+		int N = Integer.parseInt(st.nextToken());
+		for(int i = 0; i<N; i++){
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			for(int j = x; j<x+10; j++){
+				for(int k = y; k<y+10; k++){
+					map[j][k] = 1;
 				}
 			}
 		}
-		
-		int count = 0;
-		for(int i = 0; i<100; i++) {
-			for(int j = 0; j<100; j++) {
-				if(arr[i][j] == 1) {
-					count += 1;
-				}
+
+		int ans = 0;
+		for(int i = 0; i<100; i++){
+			for(int j = 0; j<100; j++){
+				ans += map[i][j];
 			}
 		}
-		
-		System.out.println(count);
-		
+
+		System.out.println(ans);
 	}
 }
