b0531edce7792fef2d1a68818aabfe4cd9b0a3ff
MinKook YOU <122504425+acd4548@users.noreply.github.com>
2024-01-26 15:44:12 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 248 ms, Memory: 13508 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String [] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = N-1; i>=0; i--){
+            for(int j = 0; j<i; j++){
+                System.out.print(" ");
+            }
+
+            for(int q = i; q<N-1; q++){
+                System.out.print("*");
+            }
+
+            System.out.print("*");
+
+            for(int q = i; q<N-1; q++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
