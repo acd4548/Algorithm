9bd92c813be3ca8684ce172c267794658075ae15
MinKook YOU <122504425+acd4548@users.noreply.github.com>
2024-01-30 22:19:08 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 2112 ms, Memory: 144492 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String [] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int [][] arr = new int [N+1][N+1];
+        long [][] prefix = new long [N+1][N+1];
+        for(int i = 1; i<N+1; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 1; j<N+1; j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                prefix[i][j] = prefix[i-1][j] + prefix[i][j-1] - prefix[i-1][j-1] + arr[i][j];
+            }
+        }
+
+
+        for(int tc = 0; tc<M; tc++){
+            st = new StringTokenizer(br.readLine());
+
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            if(x1 == x2 && y1 == y2){
+                System.out.println(arr[x1][y1]);
+            }else{
+                long num = prefix[x2][y2] - prefix[x1-1][y2] - prefix[x2][y1-1] + prefix[x1-1][y1-1];
+                System.out.println(num);
+            }
+
+        }
+    }
+}
