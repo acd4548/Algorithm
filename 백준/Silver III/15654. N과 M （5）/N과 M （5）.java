289677e2ea20a7c6962e96f7903633b4069ae9ca
MinKook YOU <122504425+acd4548@users.noreply.github.com>
2024-02-15 16:46:00 +0900

    [Silver III] Title: N과 M (5), Time: 232 ms, Memory: 31012 KB -BaekjoonHub

---
+++ 백준/Silver III/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int N, M, arr[], ans[], visited[];
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String [] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int [N+1];
+        visited = new int [N+1];
+        ans = new int [M+1];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 1; i<N+1; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        recur(1);
+        System.out.println(sb);
+    }
+
+    private static void recur(int cur){
+        if(cur > M){
+            for(int i = 1; i<=M; i++){
+                sb.append(ans[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = 1; i<N+1; i++){
+            if(visited[i] == 0){
+                visited[i] = 1;
+                ans[cur] = arr[i];
+                recur(cur+1);
+                visited[i] = 0;
+            }
+        }
+    }
+}
