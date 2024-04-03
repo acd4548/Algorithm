6648903fc91f85807563a9ba5ce7f16dfb376a4a
MinKook YOU <122504425+acd4548@users.noreply.github.com>
2024-04-03 13:11:07 +0900

    [Gold IV] Title: 최단경로, Time: 2392 ms, Memory: 116120 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -0,0 +1,80 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, E, K;
+    static ArrayList<Node>[] list;
+    static boolean[] visited;
+    static int[] dist;
+
+    static class Node {
+        int to;
+        int d;
+
+        Node(int to, int d) {
+            this.to = to;
+            this.d = d;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(br.readLine());
+        list = new ArrayList[N + 1];
+        for (int i = 0; i < N + 1; i++) {
+            list[i] = new ArrayList<>();
+        }
+
+        int v1, v2, d;
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            v1 = Integer.parseInt(st.nextToken());
+            v2 = Integer.parseInt(st.nextToken());
+            d = Integer.parseInt(st.nextToken());
+
+            list[v1].add(new Node(v2, d));
+        }
+
+        dist = new int[N + 1];
+        Arrays.fill(dist, 1 << 30);
+        dist[K] = 0;
+        visited = new boolean[N + 1];
+
+        dikj(K);
+
+        for(int i = 1; i<N+1; i++){
+            if(i == K){
+                System.out.println(0);
+            }else if(dist[i] == 1<<30){
+                System.out.println("INF");
+            }else{
+                System.out.println(dist[i]);
+            }
+        }
+    }
+
+    static void dikj(int start) {
+        int min, cur, nxt, nd;
+        for (int i = 0; i < N; i++) {
+            min = 1 << 30;
+            cur = 0;
+
+            for (int j = 1; j < N + 1; j++) {
+                if (!visited[j] && dist[j] < min) {
+                    min = dist[j];
+                    cur = j;
+                }
+            }
+
+            visited[cur] = true;
+            for (int j = 0; j < list[cur].size(); j++) {
+                nxt = list[cur].get(j).to;
+                nd = dist[cur] + list[cur].get(j).d;
+                dist[nxt] = Math.min(dist[nxt], nd);
+            }
+        }
+    }
+}
