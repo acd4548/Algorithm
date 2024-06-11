77c5fbd43a5cdac08d3b4d6b74a4db0695160af2
MinKook YOU <122504425+acd4548@users.noreply.github.com>
2024-06-11 19:20:56 +0900

    [Bronze V] Title: 엄청난 부자2, Time: 84 ms, Memory: 11844 KB -BaekjoonHub

--- 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.java
+++ 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.java
@@ -3,9 +3,10 @@ import java.util.*;
 import java.io.*;
 public class Main {
     public static void main(String [] args)throws IOException {
-        Scanner sc = new Scanner(System.in);
-        BigInteger N = sc.nextBigInteger();
-        BigInteger M = sc.nextBigInteger();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BigInteger N = new BigInteger(st.nextToken());
+        BigInteger M = new BigInteger(st.nextToken());
 
         StringBuilder sb = new StringBuilder();
         sb.append(N.divide(M)).append("\n");
