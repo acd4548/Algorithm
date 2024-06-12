7f91d7917221dae288ba1cd0fa7f141b77f7c6c4
MinKook YOU <122504425+acd4548@users.noreply.github.com>
2024-06-13 07:32:05 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 80 ms, Memory: 11704 KB -BaekjoonHub

---
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String [] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i<=N;i++){
+            for(int j = 0; j<i; j++){
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        for(int i = N-1; i>0; i--){
+            for(int j = i; j>0; j--){
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+}
