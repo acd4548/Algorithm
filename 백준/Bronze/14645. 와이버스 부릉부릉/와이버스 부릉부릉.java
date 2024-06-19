27055af59e4f4acf0067bc497c9d62d15a979800
MinKook YOU <122504425+acd4548@users.noreply.github.com>
2024-06-19 19:27:31 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 68 ms, Memory: 11516 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        for(int i = 0; i<N; i++){
+            br.readLine();
+        }
+        System.out.print("비와이");
+    }
+}
