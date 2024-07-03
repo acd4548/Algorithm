11b4991ad86bd6976b75f81c979ba0dbe13a561b
MinKook YOU <122504425+acd4548@users.noreply.github.com>
2024-07-03 15:10:31 +0900

    [D1] Title: 대각선 출력하기, Time: 112 ms, Memory: 18,672 KB -BaekjoonHub

---
+++ SWEA/D1/2027. 대각선 출력하기/대각선 출력하기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+public class Solution {
+    public static void main(String [] args)throws IOException {
+        System.out.println("#++++");
+        System.out.println("+#+++");
+        System.out.println("++#++");
+        System.out.println("+++#+");
+        System.out.println("++++#");
+    }
+}
