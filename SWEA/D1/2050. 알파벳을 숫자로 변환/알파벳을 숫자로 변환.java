f49b1a62dbd11a2ba3ca707a869f57ac2a2e059f
MinKook YOU <122504425+acd4548@users.noreply.github.com>
2024-07-03 14:54:16 +0900

    [D1] Title: 알파벳을 숫자로 변환, Time: 103 ms, Memory: 18,664 KB -BaekjoonHub

---
+++ SWEA/D1/2050. 알파벳을 숫자로 변환/알파벳을 숫자로 변환.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+public class Solution {
+    public static void main(String [] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String str = br.readLine();
+        for(int i = 0; i<str.length(); i++){
+            sb.append(str.charAt(i) - 'A' + 1).append(" ");
+        }
+        System.out.print(sb);
+    }
+}
