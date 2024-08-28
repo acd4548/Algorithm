6def095302a211720d26277215b02d5350e47650
MinKook YOU <122504425+acd4548@users.noreply.github.com>
2024-08-28 22:49:52 +0900

    [Bronze V] Title: 오늘의 날짜는?, Time: 60 ms, Memory: 11304 KB -BaekjoonHub

---
+++ 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        System.out.println("2024");
+        System.out.println("08");
+        System.out.println("28");
+    }
+}
