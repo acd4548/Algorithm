b74d5cc67dffac77f8fe3ef8e74f42789ed49341
MinKook YOU <122504425+acd4548@users.noreply.github.com>
2024-09-12 19:19:53 +0900

    [Bronze III] Title: 저작권, Time: 60 ms, Memory: 11480 KB -BaekjoonHub

---
+++ 백준/Bronze/2914. 저작권/저작권.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String [] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int I = Integer.parseInt(st.nextToken());
+        int ans = A * I - A + 1;
+        System.out.print(ans);
+    }
+}
