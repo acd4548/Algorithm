79c9af6b563dcea90cc460fef001f43f4d08d4e0
MinKook YOU <122504425+acd4548@users.noreply.github.com>
2025-01-19 23:21:13 +0900

    [Bronze V] Title: 제리와 톰, Time: 68 ms, Memory: 11500 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String [] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        System.out.print(B - A);
+        System.out.print(" ");
+        System.out.print(B);
+    }
+}
