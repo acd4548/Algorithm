9de51fc7316b79064ef83e1999581e79d4a25370
MinKook YOU <122504425+acd4548@users.noreply.github.com>
2025-02-12 14:08:28 +0900

    [Silver II] Title: 소가 길을 건너간 이유 5, Time: 88 ms, Memory: 13704 KB -BaekjoonHub

--- 백준/Silver/14465. 소가 길을 건너간 이유 5/소가 길을 건너간 이유 5.java
+++ 백준/Silver/14465. 소가 길을 건너간 이유 5/소가 길을 건너간 이유 5.java
@@ -9,21 +9,20 @@ public class Main{
         int K = Integer.parseInt(st.nextToken());
         int B = Integer.parseInt(st.nextToken());
 
-        int [] broken = new int [N+1];
+        int [] broke = new int [N+1];
+        int [] prefix = new int [N+1];
         for(int i = 0; i<B; i++){
-            int where = Integer.parseInt(br.readLine());
-            broken[where] = 1;
+            int num = Integer.parseInt(br.readLine());
+            broke[num] = 1;
+        }
+
+        for(int i = 1; i<N+1; i++){
+            prefix[i] = prefix[i-1] + broke[i];
         }
 
         int ans = Integer.MAX_VALUE;
-        for(int i = 1; i<=N - K + 1; i++){
-            int cnt = 0;
-            for(int j = i; j<i + K; j++){
-                if(broken[j] == 1){
-                    cnt++;
-                }
-            }
-            ans = Math.min(ans, cnt);
+        for(int i = K; i<N+1; i++){
+            ans = Math.min(ans, prefix[i] - prefix[i-K]);
         }
         System.out.println(ans);
     }
